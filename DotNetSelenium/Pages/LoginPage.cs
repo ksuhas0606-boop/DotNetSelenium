87d2eb109bfff80959c5d6e702e4691f6f5adba0
suhask6 <47807272+suhask6@users.noreply.github.com>
2025-11-17 19:19:50 +0530

    Add project files.

---
+++ DotNetSelenium/Pages/LoginPage.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetSelenium.Pages
+{
+    internal class LoginPage
+    {
+        public static void signUpName(IWebDriver driver,By locator, String uname)
+        {
+            driver.FindElement(locator).SendKeys(uname);
+        }
+        public static void signUpEmail(IWebDriver driver, By locator, String pass)
+        {
+            driver.FindElement(locator).SendKeys(pass);
+        }
+    }
+}
---
+++ DotNetSelenium/UnitTest1.cs
@@ -0,0 +1,97 @@
+using DotNetSelenium.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Diagnostics.Metrics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DotNetSelenium
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ValidateSignUpUserText()
+        {
+            IWebDriver driver = new ChromeDriver();
+            try
+            {
+                // Navigate to login page
+                driver.Navigate().GoToUrl("https://automationexercise.com/login");
+
+                // Maximize window
+                driver.Manage().Window.Maximize();
+
+                System.Threading.Thread.Sleep(2000);
+
+                IWebElement signupuser = driver.FindElement(By.XPath("//h2[normalize-space()='New User Signup!']"));
+
+                string actualtext = signupuser.Text;
+
+                if (actualtext.Equals("New ser signup!", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Validation Passed: 'New User Signup!' text is present.");
+                }
+                else
+                {
+                    Console.WriteLine($"Validation Failed: Expected 'New User Signup!', But Found {actualtext}");
+                }
+
+
+            }
+            catch (NoSuchElementException)
+            {
+                Console.WriteLine("Validation Failed: 'New User Signup!' text is Not present.");
+            }
+            driver.Close();
+        }
+
+        [Test]
+        public void NewUserRegister()
+        {
+            IWebDriver driver = new ChromeDriver();
+            try
+            {
+                // Navigate to login page
+                driver.Navigate().GoToUrl("https://automationexercise.com/login");
+
+                // Maximize window
+                driver.Manage().Window.Maximize();
+
+                System.Threading.Thread.Sleep(2000);
+
+                LoginPage.signUpName(driver,By.XPath("//input[@placeholder='Name']"), "Suhask";
+
+                LoginPage.signUpEmail(driver, By.XPath("//input[@placeholder='Name']"), "SuhasK");
+               
+
+                /*IWebElement entername = driver.FindElement(By.XPath("//input[@placeholder='Name']"));
+                IWebElement enterEmail = driver.FindElement(By.XPath("//input[@data-qa='signup-email']"));
+
+                entername.SendKeys("SuhasK");
+                enterEmail.SendKeys("ksuhas0606@gmail.com");
+
+                IWebElement button = driver.FindElement(By.XPath("//button[normalize-space()='Signup']"));
+                button.Click();
+
+                IWebElement headertext = driver.FindElement(By.XPath("//b[normalize-space()='Enter Account Information']"));
+                string actualtext = headertext.Text;
+
+                actualtext.Equals("Enter Account Information", StringComparison.OrdinalIgnoreCase);*/
+            }
+            catch (NoSuchElementException)
+            {
+                Console.WriteLine("Internal Error");
+            }
+            driver.Close(); 
+        }
+        [Test]
+        public void LoginUserwithincorrectemailandpassword()
+        {
+
+        }
+    }
+}
